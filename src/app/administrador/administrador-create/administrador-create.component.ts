aa9fe2a2e91760a46d0922a199755e6ffbaeb815
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-11-11 13:44:49 -0500

    AdministradorCreate listo
    Solo falta arreglar el toaster, pero los registra

--- src/app/administrador/administrador-create/administrador-create.component.ts
+++ src/app/administrador/administrador-create/administrador-create.component.ts
@@ -1,56 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import { AdministradorService } from "../administrador.service";
 import {Administrador} from "../administrador";
+import { DatePipe } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-administrador-create',
   templateUrl: './administrador-create.component.html',
-  styleUrls: ['./administrador-create.component.css']
+  styleUrls: ['./administrador-create.component.css'],
+  providers : [DatePipe]
 })
 export class AdministradorCreateComponent implements OnInit {
 
-administradorForm: FormGroup;
+a
+/**
+* Constructor for the component
+* @param dp DatePipe to format the date.
+* @param administradorService The administrador's services provider
+* @param toastrService The toastr to show messages to the user
+*/
+constructor(
+    private dp: DatePipe,
+    private administradorService: AdministradorService,
+    private toastrService: ToastrService
+) {}
 
-administradores: Administrador[];
+/**
+* The new administrador
+*/
+administrador: Administrador;
 
-  constructor(private formBuilder: FormBuilder,
-      private administradorService: AdministradorService) { 
+/**
+* The output which tells the parent component
+* that the user no longer wants to create an administrador
+*/
+@Output() cancel = new EventEmitter();
 
-        this.administradorForm= this.formBuilder.group(
-      {
-        //nombro los campos de recepcion de info
+/**
+* The output which tells the parent component
+* that the user created a new administrador
+*/
+@Output() create = new EventEmitter();
 
-        nombre:["", [Validators.required, Validators.minLength(2)]],
-        
-        correo:["", [Validators.required, Validators.email]],
+/**
+* Creates an administrador
+*/
+createAdministrador(): Administrador {
 
-        username:["", [Validators.required]],
+    console.log(this.administrador);
+    this.administradorService.createAdministrador(this.administrador)
+        .subscribe((administrador) => {
+            this.administrador = administrador;
+            this.create.emit();
+            this.toastrService.success("The administrador was created", "administrador creation");
 
-        contrasena:["",[Validators.required]]
+        });
+    return this.administrador;
+}
 
+/**
+* Emits the signal to tell the parent component that the
+* user no longer wants to create an user
+*/
+cancelCreation(): void {
+    this.cancel.emit();
+}
 
-      }
-    )
-      }
+/**
+* This function will initialize the component
+*/
+ngOnInit() {
+    this.administrador = new Administrador();
+}
 
-      createAdministradodr(newAdministrador: Administrador) {
-    // Process checkout data here
-    console.warn("Your order has been submitted", newAdministrador);
-  this.administradorService.createAdministrador(newAdministrador).subscribe(administrador => {
-      this.administradores.push(administrador);
-    });
-
-   this.administradorForm.reset();
-   }
-
-
-
-  ngOnInit() {
-
-       this.administradorService
-      .getAdministradores()
-      .subscribe(administradores => (this.administradores = administradores));
-  }
 
 }
