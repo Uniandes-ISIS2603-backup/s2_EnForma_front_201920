7c7ed536dfae4586990f66f4fe70a2c04068fbbb
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-11-11 01:41:04 -0500

    AdministradorDetailListo

--- src/app/administrador/administrador-detail/administrador-detail.component.ts
+++ src/app/administrador/administrador-detail/administrador-detail.component.ts
@@ -12,42 +12,48 @@ import { AdministradorDetail } from '../administrador-detail';
   styleUrls: ['./administrador-detail.component.css']
 })
 export class AdministradorDetailComponent implements OnInit {
-  administradorDetail: AdministradorDetail;
-
-  @Input() administrador_id: number;
-
-  loader:any;
-
-
-  constructor(private administradorService: AdministradorService,
-    private route: ActivatedRoute) { }
-
   /**
-  * The method which retrieves the books of an editorial
-  */
-  getAdministradorDetail(): void {
-    this.administradorService.getAdministradorDetail(this.administrador_id)
-      .subscribe(administradorDetail => {
-        this.administradorDetail = administradorDetail
-      });
-  }
+    * The administrador
+    */
+   @Input() administradorDetail: AdministradorDetail;
+   /**
+   * Constructor for the component
+   * @param route The route which helps to retrieves the id of the book to be shown
+   * @param administradorService The administrador's services provider
+   * @param toastrService The toastr to show messages to the user
+   */
+   constructor(
+       private route: ActivatedRoute,
+       private administradorService: AdministradorService 
+   ) { }
 
+   
+   
 
-  onLoad(params) {
-    this.administrador_id = parseInt(params["id"]);
-    this.administradorDetail = new AdministradorDetail();
-    this.getAdministradorDetail();
-  }
+   /**
+   * El id del administrador que viene en el path get .../administradors/administrador_id
+   */
+   administrador_id: number;
+   /**
+   * The method which obtains the administrador whose details we want to show
+   */
+   getAdministradorDetail(): void {
+       this.administradorService.getAdministradorDetail(this.administrador_id)
+           .subscribe(administradorDetail => {
+               this.administradorDetail = administradorDetail
+           });
+   }
 
-
-
-  ngOnInit() {
-    this.loader = this.route.params.subscribe((params: Params) =>
-      this.onLoad(params)
-    );
-  }
-
-  ngOnDestroy() {
-    this.loader.unsubscribe();
-  }
+  
+   /**
+   * The method which initializes the component.
+   * We need to create the administrador so it is never considered as undefined
+   */
+   ngOnInit() {
+       this.administrador_id = +this.route.snapshot.paramMap.get('id');
+       if (this.administrador_id){
+       this.administradorDetail = new AdministradorDetail();
+       this.getAdministradorDetail();
+       }
+   }
 }
