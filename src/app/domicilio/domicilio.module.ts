e23f1ace476e54b0a72483b4dec74ca7f196a186
juan sebastian clavijo <js.clavijos@uniandes.edu.co>
2019-11-11 20:01:20 -0500

    Domicilio create listoo

--- src/app/domicilio/domicilio.module.ts
+++ src/app/domicilio/domicilio.module.ts
@@ -3,19 +3,23 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { DomicilioListComponent } from './domicilio-list/domicilio-list.component';
 
+import { FormsModule } from '@angular/forms';
+
 import { DomicilioService } from './domicilio.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DomicilioDetailComponent } from './domicilio-detail/domicilio-detail.component';
 import { DomicilioCreateComponent } from './domicilio-create/domicilio-create.component';
 
+
 @NgModule({
   imports: [
       CommonModule,
       ReactiveFormsModule,
+      FormsModule,
       AppRoutingModule
   ],
   declarations: [DomicilioDetailComponent, DomicilioListComponent, DomicilioCreateComponent],
   providers: [DomicilioService],
-  exports: [DomicilioListComponent]
+  exports: [DomicilioListComponent, DomicilioCreateComponent]
 })
 export class DomicilioModule { }
