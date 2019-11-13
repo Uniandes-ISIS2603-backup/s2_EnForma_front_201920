87b9f261a821802717741a777ce8e2416b096c4d
juan sebastian clavijo <js.clavijos@uniandes.edu.co>
2019-11-13 15:34:15 -0500

    problema del object arreglado

--- src/app/domicilio/domicilio-detail.ts
+++ src/app/domicilio/domicilio-detail.ts
@@ -1,10 +1,8 @@
 
 import {Domicilio} from "./domicilio";
 //import {QuejasYReclamos} from '../quejasYReclamos/quejasYReclamos';
-import {Pago} from '../pago/pago';
 
 export class DomicilioDetail extends Domicilio{
 
   //quejasYReclamos: QuejasYReclamos;
-  pago: Pago;
 }
--- src/app/domicilio/domicilio.ts
+++ src/app/domicilio/domicilio.ts
@@ -1,5 +1,7 @@
 import {Cliente} from '../cliente/cliente';
 import {ComidaTipo } from '../comidatipo/comida';
+import {Pago} from '../pago/pago';
+
 export class Domicilio 
 {
   id: number;
@@ -8,4 +10,5 @@ export class Domicilio
   costo: number;
   cliente: Cliente;
   comidaTipo: ComidaTipo;
+  pago: Pago;
 }
