0642dee969adda77497ae2f4e9e90e870517c2f8
Elina Jaimes <ev.jaimes@uniandes.edu.co>
2019-12-05 05:31:34 -0500

    PagoCreate
    completamente dependiente de Domicilio

--- src/app/pago/pago.service.ts
+++ src/app/pago/pago.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Pago } from "./pago";
 import { Observable } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
 
 
 const API_URL = "http://localhost:8080/s2_enforma-api/api/";
@@ -14,8 +15,15 @@ export class PagoService {
 
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })};
+
   getPagos(): Observable<Pago[]> {
     return this.http.get<Pago[]>(API_URL + pagos);
+  }
+
+  createPago(pago: Pago): Observable<Pago> {
+    return this.http.post<Pago>(API_URL+pagos, Pago, this.httpOptions).pipe(tap((Pago: Pago) => console.log(`added Pago w/ id=${Pago.id}`)));
   }
 
 }
